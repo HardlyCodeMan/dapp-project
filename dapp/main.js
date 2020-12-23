bf28e53f122915b326887ad39a4a28ad134de743
HardlyCodeMan <graham@planbit.solutions>
2020-12-23 14:33:09 +0800

    Add: modifications for CoinFlip

--- dapp/main.js
+++ dapp/main.js
@@ -3,44 +3,50 @@ var contractInstance;
 
 $(document).ready(function() {
     window.ethereum.enable().then(function(accounts) {
-        contractInstance = new web3.eth.Contract(abi, "0x46852b33798EDecCE021aba7131d93E5A8c64A72", {from: accounts[0]});
-        //console.log(contractInstance);
+        contractInstance = new web3.eth.Contract(abi, "0xA298622F83f7781f565B39b1BFb97344D67E2251", {from: accounts[0]});
+        console.log(contractInstance);
     });
-    $("#add_data_button").click(inputData);
-    $("#get_data_button").click(fetchAndDisplay);
+    $("#heads_button").click(newFlip(0));
+    $("#tails_button").click(newFlip(1));
+    $("#results_button").click(fetchAndDisplay);
 });
 
-function inputData() {
+function newFlip(_expectedInt) {
     // Form inputs
-    var name = $("#name_input").val();
-    var age = $("#age_input").val();
-    var height = $("#height_input").val();
+    var _value = $("#value_input").val();
+    var _expected;
+
+    if(_expectedInt = 0) {
+        _expected = "Heads";
+    } else {
+        _expected = "Tails";
+    }
     
     // Metamask signing params
     var sendConfig = {
-        value: web3.utils.toWei("1", "ether")
+        value: web3.utils.toWei(_value, "ether")
     };
 
     // Let Metamask send the transaction
-    contractInstance.methods.createPerson(name, age, height).send(sendConfig)
+    contractInstance.methods.newCoinFlip(_expected, _expectedInt).send(sendConfig)
         // Gett the tx hash
         .on("transactionHash", function(hash) {
-            //console.log("Tx Hash: " + hash);
+            console.log("Tx Hash: " + hash);
         })
         // Get tx confirmations, min 12 recommended for mainnet
         .on("confirmation", function(confirmationNr) {
-            //console.log("Confirmations: " + confirmationNr);
+            console.log("Confirmations: " + confirmationNr);
         })
         // Get tx receipt, outcome & state changes of the tx
         .on("receipt", function(receipt) {
-            //console.log("Receipt: " + JSON.stringify(receipt));
+            console.log("Receipt: " + JSON.stringify(receipt));
         })
 }
 
 function fetchAndDisplay() {
     // Get the data from the blockchain
     contractInstance.methods.getPerson().call().then(function(result) {
-        //console.log(result);
+        console.log(result);
 
         // Set the HTML elements
         $("#name_output").text(result.name);
