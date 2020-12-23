1e176b92fae0f3f3e0be074e2c5d5e6756907604
HardlyCodeMan <graham@planbit.solutions>
2020-12-23 14:32:45 +0800

    Update: new abi data

--- dapp/abi.js
+++ dapp/abi.js
@@ -4,18 +4,12 @@ var abi = [
     "inputs": [
       {
         "indexed": false,
-        "internalType": "string",
-        "name": "name",
-        "type": "string"
-      },
-      {
-        "indexed": false,
-        "internalType": "bool",
-        "name": "senior",
-        "type": "bool"
+        "internalType": "uint256",
+        "name": "_value",
+        "type": "uint256"
       }
     ],
-    "name": "personCreated",
+    "name": "eventWithdrawAll",
     "type": "event"
   },
   {
@@ -23,35 +17,175 @@ var abi = [
     "inputs": [
       {
         "indexed": false,
-        "internalType": "string",
-        "name": "name",
-        "type": "string"
+        "internalType": "address",
+        "name": "_address",
+        "type": "address"
       },
       {
         "indexed": false,
-        "internalType": "bool",
-        "name": "senior",
-        "type": "bool"
+        "internalType": "uint256",
+        "name": "_value",
+        "type": "uint256"
+      }
+    ],
+    "name": "newDepositEvent",
+    "type": "event"
+  },
+  {
+    "anonymous": false,
+    "inputs": [
+      {
+        "indexed": false,
+        "internalType": "uint256",
+        "name": "_id",
+        "type": "uint256"
       },
       {
         "indexed": false,
         "internalType": "address",
-        "name": "deletedBy",
+        "name": "_user",
         "type": "address"
+      },
+      {
+        "indexed": false,
+        "internalType": "string",
+        "name": "_expected",
+        "type": "string"
+      },
+      {
+        "indexed": false,
+        "internalType": "uint256",
+        "name": "_expectedInt",
+        "type": "uint256"
+      },
+      {
+        "indexed": false,
+        "internalType": "uint256",
+        "name": "_value",
+        "type": "uint256"
       }
     ],
-    "name": "personDeleted",
+    "name": "newFlipEvent",
+    "type": "event"
+  },
+  {
+    "anonymous": false,
+    "inputs": [
+      {
+        "indexed": false,
+        "internalType": "uint256",
+        "name": "_id",
+        "type": "uint256"
+      },
+      {
+        "indexed": false,
+        "internalType": "address",
+        "name": "_user",
+        "type": "address"
+      },
+      {
+        "indexed": false,
+        "internalType": "string",
+        "name": "_result",
+        "type": "string"
+      },
+      {
+        "indexed": false,
+        "internalType": "uint256",
+        "name": "_resultInt",
+        "type": "uint256"
+      },
+      {
+        "indexed": false,
+        "internalType": "bool",
+        "name": "_win",
+        "type": "bool"
+      },
+      {
+        "indexed": false,
+        "internalType": "uint256",
+        "name": "_value",
+        "type": "uint256"
+      }
+    ],
+    "name": "newFlipResultEvent",
+    "type": "event"
+  },
+  {
+    "anonymous": false,
+    "inputs": [
+      {
+        "indexed": false,
+        "internalType": "address",
+        "name": "_to",
+        "type": "address"
+      },
+      {
+        "indexed": false,
+        "internalType": "address",
+        "name": "_from",
+        "type": "address"
+      },
+      {
+        "indexed": false,
+        "internalType": "uint256",
+        "name": "_value",
+        "type": "uint256"
+      }
+    ],
+    "name": "newWithdrawlEvent",
     "type": "event"
   },
   {
     "constant": true,
-    "inputs": [],
-    "name": "balance",
-    "outputs": [
+    "inputs": [
       {
         "internalType": "uint256",
         "name": "",
         "type": "uint256"
+      }
+    ],
+    "name": "completedFlips",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "id",
+        "type": "uint256"
+      },
+      {
+        "internalType": "address",
+        "name": "player",
+        "type": "address"
+      },
+      {
+        "internalType": "string",
+        "name": "expected",
+        "type": "string"
+      },
+      {
+        "internalType": "uint256",
+        "name": "expectedInt",
+        "type": "uint256"
+      },
+      {
+        "internalType": "string",
+        "name": "result",
+        "type": "string"
+      },
+      {
+        "internalType": "uint256",
+        "name": "resultInt",
+        "type": "uint256"
+      },
+      {
+        "internalType": "uint256",
+        "name": "value",
+        "type": "uint256"
+      },
+      {
+        "internalType": "bool",
+        "name": "win",
+        "type": "bool"
       }
     ],
     "payable": false,
@@ -78,103 +212,82 @@ var abi = [
     "inputs": [
       {
         "internalType": "string",
-        "name": "name",
+        "name": "_expected",
         "type": "string"
       },
       {
         "internalType": "uint256",
-        "name": "age",
-        "type": "uint256"
-      },
-      {
-        "internalType": "uint256",
-        "name": "height",
+        "name": "_expectedInt",
         "type": "uint256"
       }
     ],
-    "name": "createPerson",
+    "name": "newCoinFlip",
     "outputs": [],
     "payable": true,
     "stateMutability": "payable",
     "type": "function"
   },
   {
-    "constant": true,
-    "inputs": [],
-    "name": "getPerson",
-    "outputs": [
+    "constant": false,
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "_id",
+        "type": "uint256"
+      },
+      {
+        "internalType": "address",
+        "name": "_user",
+        "type": "address"
+      },
       {
         "internalType": "string",
-        "name": "name",
+        "name": "_expected",
         "type": "string"
       },
       {
         "internalType": "uint256",
-        "name": "age",
+        "name": "_expectedInt",
         "type": "uint256"
       },
       {
         "internalType": "uint256",
-        "name": "height",
+        "name": "_value",
         "type": "uint256"
-      },
-      {
-        "internalType": "bool",
-        "name": "senior",
-        "type": "bool"
       }
     ],
-    "payable": false,
-    "stateMutability": "view",
-    "type": "function"
-  },
-  {
-    "constant": false,
-    "inputs": [
+    "name": "randomFlip",
+    "outputs": [
       {
-        "internalType": "address",
-        "name": "creator",
-        "type": "address"
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
       }
     ],
-    "name": "deletePerson",
-    "outputs": [],
     "payable": false,
     "stateMutability": "nonpayable",
     "type": "function"
   },
   {
-    "constant": true,
-    "inputs": [
+    "constant": false,
+    "inputs": [],
+    "name": "deposit",
+    "outputs": [
       {
         "internalType": "uint256",
-        "name": "index",
+        "name": "",
         "type": "uint256"
       }
     ],
-    "name": "getCreator",
-    "outputs": [
-      {
-        "internalType": "address",
-        "name": "",
-        "type": "address"
-      }
-    ],
-    "payable": false,
-    "stateMutability": "view",
+    "payable": true,
+    "stateMutability": "payable",
     "type": "function"
   },
   {
     "constant": false,
     "inputs": [],
-    "name": "withdrawAll",
-    "outputs": [
-      {
-        "internalType": "uint256",
-        "name": "",
-        "type": "uint256"
-      }
-    ],
+    "name": "withdrawlAll",
+    "outputs": [],
     "payable": false,
     "stateMutability": "nonpayable",
     "type": "function"
